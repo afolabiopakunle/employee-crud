4f2ede569c8468be0af5475ce2b1e1444dd8f758
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-06-18 12:43:41 +0100

    router.snapshot.paramMap.get('id') works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditComponent } from './pages/employees/edit/edit.component';
 import { EmployeesComponent } from './pages/employees/employees.component';
 
 const routes: Routes = [
-  {path: '', component: EmployeesComponent}
+  { path: '', component: EmployeesComponent },
+  { path: 'employee/add/:id', component: EditComponent },
+  { path: 'employee/edit/:id', component: EditComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { EmployeesComponent } from './pages/employees/employees.component';
+import { EditComponent } from './pages/employees/edit/edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
